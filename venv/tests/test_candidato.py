3e05650fb0764ca8d9d1463c33aee0d46ac6403d
Agner <65578473+AgnerLoss@users.noreply.github.com>
2025-02-22 00:30:32 -0300

    Delete venv directory

--- venv/tests/test_candidato.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.candidato import Candidato
-
-def test_candidato_corresponde_ao_concurso():
-    candidato = Candidato("Teste", "01/01/2000", "123.456.789-00", ["carpinteiro"])
-    assert candidato.corresponde_ao_concurso(["carpinteiro", "marceneiro"]) == True
-    assert candidato.corresponde_ao_concurso(["professor"]) == False
--- venv/tests/test_concurso.py
+++
@@ -1,8 +0,0 @@
-import pytest # type: ignore
-from src.concurso import Concurso
-
-def test_concurso_corresponde_ao_candidato():
-    concurso = Concurso("SEJUS", "1/2023", "12345", ["carpinteiro"])
-    assert concurso.corresponde_ao_candidato(["carpinteiro", "marceneiro"]) == True
-    assert concurso.corresponde_ao_candidato(["professor"]) == False
-
