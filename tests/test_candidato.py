58d09dc6f5e66f8eabee1edb9fdcc879383038e1
AgnerLoss <agner1984@yahoo.com.br>
2025-02-22 15:01:14 -0300

    update testes

--- tests/test_candidato.py
+++ tests/test_candidato.py
@@ -3,31 +3,31 @@ from src.candidato import Candidato
 
 def test_candidato_corresponde_ao_concurso():
     # Caso 1: Há correspondência entre as profissões do candidato e as vagas do concurso
-    candidato = Candidato("Teste", "01/01/2000", "123.456.789-00", ["carpinteiro"])
+    candidato = Candidato("Lindsey Craft", "19/05/1976", "182.845.084-34", ["carpinteiro"])
     assert candidato.corresponde_ao_concurso(["carpinteiro", "marceneiro"]) == True
 
     # Caso 2: Não há correspondência
-    assert candidato.corresponde_ao_concurso(["professor"]) == False
+    assert candidato.corresponde_ao_concurso(["engenheiro"]) == False
 
     # Caso 3: Lista de vagas do concurso está vazia
     assert candidato.corresponde_ao_concurso([]) == False
 
     # Caso 4: Lista de profissões do candidato está vazia
-    candidato_sem_profissoes = Candidato("Teste", "01/01/2000", "123.456.789-00", [])
+    candidato_sem_profissoes = Candidato("Lindsey Craft", "19/05/1976", "182.845.084-34", [])
     assert candidato_sem_profissoes.corresponde_ao_concurso(["carpinteiro"]) == False
 
     # Caso 5: Múltiplas correspondências
-    candidato_multiplas_profissoes = Candidato("Teste", "01/01/2000", "123.456.789-00", ["carpinteiro", "marceneiro"])
+    candidato_multiplas_profissoes = Candidato("Cory Mendoza", "11/02/1957", "565.512.353-92", ["carpinteiro", "marceneiro"])
     assert candidato_multiplas_profissoes.corresponde_ao_concurso(["carpinteiro", "pedreiro"]) == True
 
     # Caso 6: Nenhuma correspondência, mas as listas não estão vazias
     assert candidato_multiplas_profissoes.corresponde_ao_concurso(["engenheiro", "arquiteto"]) == False
 
 def test_candidato_de_linha():
-    # Testar o método de_linha (se existir)
-    linha = "Teste;01/01/2000;123.456.789-00;carpinteiro,marceneiro"
+    # Testar o método de_linha
+    linha = "Lindsey Craft\t19/05/1976\t182.845.084-34\t['carpinteiro']"
     candidato = Candidato.de_linha(linha)
-    assert candidato.nome == "Teste"
-    assert candidato.data_nascimento == "01/01/2000"
-    assert candidato.cpf == "123.456.789-00"
-    assert candidato.profissoes == ["carpinteiro", "marceneiro"]
+    assert candidato.nome == "Lindsey Craft"
+    assert candidato.data_nascimento == "19/05/1976"
+    assert candidato.cpf == "182.845.084-34"
+    assert candidato.profissoes == ["carpinteiro"]
--- tests/test_concurso.py
+++ tests/test_concurso.py
@@ -3,31 +3,31 @@ from src.concurso import Concurso
 
 def test_concurso_corresponde_ao_candidato():
     # Caso 1: Há correspondência entre as profissões do candidato e as vagas do concurso
-    concurso = Concurso("SEJUS", "1/2023", "12345", ["carpinteiro"])
+    concurso = Concurso("SEJUS", "15/2017", "61828450843", ["carpinteiro", "professor de matemática", "assistente administrativo"])
     assert concurso.corresponde_ao_candidato(["carpinteiro", "marceneiro"]) == True
 
     # Caso 2: Não há correspondência
-    assert concurso.corresponde_ao_candidato(["professor"]) == False
+    assert concurso.corresponde_ao_candidato(["engenheiro"]) == False
 
     # Caso 3: Lista de vagas do concurso está vazia
-    concurso_sem_vagas = Concurso("SEJUS", "1/2023", "12345", [])
+    concurso_sem_vagas = Concurso("SEJUS", "15/2017", "61828450843", [])
     assert concurso_sem_vagas.corresponde_ao_candidato(["carpinteiro"]) == False
 
     # Caso 4: Lista de profissões do candidato está vazia
     assert concurso.corresponde_ao_candidato([]) == False
 
     # Caso 5: Múltiplas correspondências
-    concurso_multiplas_vagas = Concurso("SEJUS", "1/2023", "12345", ["carpinteiro", "marceneiro"])
+    concurso_multiplas_vagas = Concurso("SEJUS", "15/2017", "61828450843", ["carpinteiro", "marceneiro"])
     assert concurso_multiplas_vagas.corresponde_ao_candidato(["carpinteiro", "pedreiro"]) == True
 
     # Caso 6: Nenhuma correspondência, mas as listas não estão vazias
     assert concurso_multiplas_vagas.corresponde_ao_candidato(["engenheiro", "arquiteto"]) == False
 
 def test_concurso_de_linha():
-    # Testar o método de_linha (se existir)
-    linha = "SEJUS;1/2023;12345;carpinteiro,marceneiro"
+    # Testar o método de_linha
+    linha = "SEJUS\t15/2017\t61828450843\t['carpinteiro', 'professor de matemática', 'assistente administrativo']"
     concurso = Concurso.de_linha(linha)
     assert concurso.orgao == "SEJUS"
-    assert concurso.edital == "1/2023"
-    assert concurso.codigo == "12345"
-    assert concurso.vagas == ["carpinteiro", "marceneiro"]
+    assert concurso.edital == "15/2017"
+    assert concurso.codigo == "61828450843"
+    assert concurso.vagas == ["carpinteiro", "professor de matemática", "assistente administrativo"]
